4774e1e5f5429a9b902dba5d7a7b12dd01bc20a6
Adrian Gasiorek <adrian.gasiorek@icloud.com>
2022-08-04 19:11:50 +0200

    Add container

---
+++ src/components/List/List.js
@@ -0,0 +1,19 @@
+import styles from './List.module.scss';
+import Column from '../Column/Column';
+const List = () => {
+    return (
+      <div>
+        <header className={styles.header}>
+            <h2 className={styles.title}>Things to do<span>soon</span></h2>
+        </header>
+        <p className={styles.description}>Intresting things i want to check out</p>
+        <section  className={styles.columns}>
+            <Column title='Books' icon='book'/>
+            <Column title='Movies' icon='gamepad'/>
+            <Column title='Games' icon='film'/>
+        </section>
+      </div>
+    );
+  };
+
+  export default List;
